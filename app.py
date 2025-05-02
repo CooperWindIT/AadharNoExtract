0c0ab9270126db51949ccbc4af58e5c8373f940d
CooperWindIT <uisupport@cooperwind.in>
2025-05-02 16:56:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ def extract_aadhaar():
 
 if __name__ == '__main__':
     print("Starting server on http://127.0.0.1:5000")
-    serve(app, host='127.0.0.1', port=5000)
+    serve(app, host='0.0.0.0', port=5000)
